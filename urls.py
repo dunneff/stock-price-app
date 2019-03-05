ccb601a8c7a2938c880cd9bb942f05f770152bcb
Friedrich Dunne <dunneff@tcd.ie>
2019-03-05 16:46:52 +0000

    Removed urls.py file that was in the wrong directory

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-
-from stock_price_app/views/test import test_view
-
-urlpatterns = [
-        url(r'^$', test_view),
-        url(r'^admin/', admin.site.urls),
-        ]
